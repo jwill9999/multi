32444bfde1738728f4c33a3d5bcb7ba4f4a32ba0
Jason Williams <jasonwilliams2308@gmail.com>
2024-03-30 16:05:05 +0000

    add microservices and sql dependancies

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,16 +5,20 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+    transport: Transport.TCP,
+    options: {
+        port: 3000
+    }
+    });
+    app.listen();
+
+  
 }
 
 bootstrap();
