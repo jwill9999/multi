b4495d12dac61cdd1e6d5ff5ab357bb7b2a83eaa
Jason Williams <jasonwilliams2308@gmail.com>
2024-03-30 16:39:27 +0000

    Create Microservice Modules

---
+++ apps/api/src/app/service-b/service-b.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('service-b')
+export class ServiceBController {}
---
+++ apps/api/src/app/service-b/service-b.module.ts
@@ -0,0 +1,10 @@
+// service-b.module.ts
+import { Module } from '@nestjs/common';
+import { ServiceBController } from './service-b.controller';
+import { ServiceBService } from './service-b.service';
+
+@Module({
+  controllers: [ServiceBController],
+  providers: [ServiceBService],
+})
+export class ServiceAModule {}
